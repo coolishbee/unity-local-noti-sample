eb772ae8e6f86efd0181919ae0ffeb6954f8fb02
jameschun7 <chc3484@gmail.com>
2021-09-24 17:37:02 +0900

    start

---
+++ Assets/Scripts/MainObject.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Notifications.Android;
+
+public class MainObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var c = new AndroidNotificationChannel()
+        {
+            Id = "123",
+            Name = "Default Channel",
+            Importance = Importance.High,
+            Description = "Generic notifications",
+        };
+        AndroidNotificationCenter.RegisterNotificationChannel(c);
+
+
+        AndroidNotificationCenter.NotificationReceivedCallback receivedNotificationHandler =
+            delegate (AndroidNotificationIntentData data)
+            {
+                var msg = "Notification received : " + data.Id + "\n";
+                msg += "\n Notification received: ";
+                msg += "\n .Title: " + data.Notification.Title;
+                msg += "\n .Body: " + data.Notification.Text;
+                msg += "\n .Channel: " + data.Channel;
+                Debug.Log(msg);
+            };
+
+        AndroidNotificationCenter.OnNotificationReceived += receivedNotificationHandler;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClickPushButton()
+    {
+#if UNITY_ANDROID
+        var notification = new AndroidNotification();
+        notification.Title = "SomeTitle";
+        notification.Text = "SomeText";
+        notification.FireTime = System.DateTime.Now.AddSeconds(5);
+
+        AndroidNotificationCenter.SendNotification(notification, "123");
+#elif UNITY_IOS
+
+#endif
+    }
+}
